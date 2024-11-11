76c76cb9f058fa365150669d992d89dfdc0eeb2a
diogo-dantas <diogo.dantas@live.com>
2024-11-11 16:40:34 -0300

    Feito metodo para coleta de métricas no banco de dados.

--- scripts/monitoramento/monitor_postgres.py
+++ scripts/monitoramento/monitor_postgres.py
@@ -21,3 +21,18 @@ class PostgresMetricsCollector:
 			)
 		except Exception as e:
 			print("Erro ao conectar ao banco de dados: ", e)
+
+	def collect_metrics(self):
+		if not self.conn:
+			self.connect()
+
+		cursor = self.conn.cursor()
+		cursor.execute("SELECT * FROM pg_stat_activity;")
+		data = cursor.fetchall()
+		columns = [desc[0] for desc in cursor.description]
+		df = pd.DataFrame(data, columns = columns)
+		cursor.close()
+		return df
+
+	# O método collect_metrics consulta a tabela pg_stat_activity para obter informações sobre as conexões ativas 
+	# e atividades atuais no banco de dados, retornando os dados em um DataFrame do Pandas.
