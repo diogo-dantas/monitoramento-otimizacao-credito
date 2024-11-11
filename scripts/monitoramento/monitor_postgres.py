7fd6317e0cabd480bf2bb97752a6637d935ab720
diogo-dantas <diogo.dantas@live.com>
2024-11-11 17:01:29 -0300

    Refatorado construtor e método de conexão com uso das credencias em dicionário ao invés de atributos e tratamento de exceções.

--- scripts/monitoramento/monitor_postgres.py
+++ scripts/monitoramento/monitor_postgres.py
@@ -2,25 +2,41 @@ import psycopg2
 import pandas as pd
 
 class PostgresMetricsCollector:
-	def __init__(self, dbname, user, password, host, port):
+	def __init__(
 		self.conn = None
-		self.dbname = dbname
-		self.user = user
-		self.password = password
-		self.host = host 
-		self. port = port
+		dbname: str,
+        user: str,
+        password: str,
+        host: str = 'localhost',
+        port: str = '5432'
+        ):
+
+	# Validação de None
+    if any(param is None for param in [dbname, user, password]):
+        raise TypeError("Credenciais do banco não podem ser None")
+    
+    # Validação de tipo string
+    if not all(isinstance(param, str) for param in [dbname, user, password, host, port]):
+        raise TypeError("Credenciais do banco devem ser strings")
+    
+    """ Inicializa o conector com PostgreSQL """
+
+    self.credentials = {
+        'dbname': dbname,
+        'user': user,
+        'password': password,
+        'host': host,
+        'port': port
+    }
+
 
 	def connect(self):
 		try:
-			self.conn = psycopg2.connect(
-			dbname = self.dbname,
-			user = self.user,
-			password = self.password,
-			host = self.host,
-			port = self.port
-			)
+            conn = psycopg2.connect(**self.credentials)
+            return conn
 		except Exception as e:
 			print("Erro ao conectar ao banco de dados: ", e)
+
 
 	def collect_metrics(self):
 		if not self.conn:
@@ -36,3 +52,13 @@ class PostgresMetricsCollector:
 
 	# O método collect_metrics consulta a tabela pg_stat_activity para obter informações sobre as conexões ativas 
 	# e atividades atuais no banco de dados, retornando os dados em um DataFrame do Pandas.
+
+	def close_connection(self):
+		if self.conn:
+			self.conn.close()
+
+if __name__ == "__main__":
+	collector = PostgresMetricsCollector(
+		dbname = bank_customers
+		)
+
